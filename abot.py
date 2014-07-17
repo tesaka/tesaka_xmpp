029fcf1554d26ea2103c642ce7a5471c316bd2ad
tesaka <tesaka@ashisuto.co.jp>
2014-07-17 16:37:49 +0900

    utf8

--- abot.py
+++ abot.py
@@ -1,48 +1,28 @@
 # -*- coding:utf-8 -*-
-<<<<<<< HEAD
 #XMPPãƒ¡ãƒƒã‚»ãƒ¼ã‚¸ã‚’å—ä¿¡ã—ãŸã‚‰ç‰¹å®šæ–‡å­—åˆ—ã‚’è¿”ã™ã‚µãƒ³ãƒ—ãƒ«
 
 #xmppãƒ©ã‚¤ãƒ–ãƒ©ãƒªã®ã‚¤ãƒ³ãƒãƒ¼ãƒˆ
 import xmpp 
 
 #JIDã‚’æŒ‡å®š
-=======
-#XMPPƒƒbƒZ[ƒW‚ðŽóM‚µ‚½‚ç“Á’è•¶Žš—ñ‚ð•Ô‚·ƒTƒ“ƒvƒ‹
-
-#xmppƒ‰ƒCƒuƒ‰ƒŠ‚ÌƒCƒ“ƒ|[ƒg
-import xmpp 
-
-#JID‚ðŽw’è
->>>>>>> fd904b3cedf18d496782c1023722d72c3c01bd1c
 user="tesaka2@altair" 
 password="tesaka2" 
 server="altair"
 jid = xmpp.JID(user) 
  
-<<<<<<< HEAD
 #ãƒãƒ³ãƒ‰ãƒ©ã®å®šç¾©
-=======
-#ƒnƒ“ƒhƒ‰‚Ì’è‹`
->>>>>>> fd904b3cedf18d496782c1023722d72c3c01bd1c
 def message_handler(connect_object, message_node): 
 
 	message	=	"Welcome to my first Bot:)"
 	connect_object.send( xmpp.Message( message_node.getFrom(),message)) 
 
-<<<<<<< HEAD
 #XMPPã‚µãƒ¼ãƒã«æŽ¥ç¶š
 connection = xmpp.Client(server,debug=[]) 
 #debugãƒ¢ãƒ¼ãƒ‰
-=======
-#XMPPƒT[ƒo‚ÉÚ‘±
-connection = xmpp.Client(server,debug=[]) 
-#debugƒ‚[ƒh
->>>>>>> fd904b3cedf18d496782c1023722d72c3c01bd1c
 #connection = xmpp.Client(server) 
 connection.connect() 
 result = connection.auth(jid.getNode(), password, "abot-client")
 
-<<<<<<< HEAD
 #messageã‚¹ã‚¿ãƒ³ã‚¶ã‚’å—ä¿¡ã—ãŸã‚‰ãƒãƒ³ãƒ‰ãƒ©ãŒå‹•ä½œã™ã‚‹
 connection.RegisterHandler('message', message_handler) 
 
@@ -50,15 +30,6 @@ connection.RegisterHandler('message', message_handler)
 connection.sendInitPresence() 
 
 #Pythonã‚’ç¶™ç¶šç¨¼å‹•ã•ã›ã‚‹ãŸã‚ã«ç„¡é™ãƒ«ãƒ¼ãƒ—
-=======
-#messageƒXƒ^ƒ“ƒU‚ðŽóM‚µ‚½‚çƒnƒ“ƒhƒ‰‚ª“®ì‚·‚é
-connection.RegisterHandler('message', message_handler) 
-
-#PresenseƒXƒ^ƒ“ƒU‚Ì‘—M
-connection.sendInitPresence() 
-
-#Python‚ðŒp‘±‰Ò“®‚³‚¹‚é‚½‚ß‚É–³ŒÀƒ‹[ƒv
->>>>>>> fd904b3cedf18d496782c1023722d72c3c01bd1c
 while connection.Process(1):
 
 	pass
--- cbot.py
+++ cbot.py
@@ -1,37 +1,22 @@
 # -*- coding:utf-8 -*-
-<<<<<<< HEAD
 #XMPPãƒ¡ãƒƒã‚»ãƒ¼ã‚¸ã‚’å—ä¿¡ã—ãŸã‚‰ä»»æ„æ–‡å­—ã‚’è¿”ä¿¡ã™ã‚‹ã‚µãƒ³ãƒ—ãƒ«(é€£ç¶šå—ä¿¡å¯èƒ½)
 
 #ãƒ©ã‚¤ãƒ–ãƒ©ãƒªã®ã‚¤ãƒ³ãƒãƒ¼ãƒˆ
-=======
-#XMPPƒƒbƒZ[ƒW‚ðŽóM‚µ‚½‚ç”CˆÓ•¶Žš‚ð•ÔM‚·‚éƒTƒ“ƒvƒ‹(˜A‘±ŽóM‰Â”\)
-
-#ƒ‰ƒCƒuƒ‰ƒŠ‚ÌƒCƒ“ƒ|[ƒg
->>>>>>> fd904b3cedf18d496782c1023722d72c3c01bd1c
 import threading
 import sys
 import xmpp 
 
-<<<<<<< HEAD
 #JIDã‚’æŒ‡å®š
-=======
-#JID‚ðŽw’è
->>>>>>> fd904b3cedf18d496782c1023722d72c3c01bd1c
 user="tesaka2@altair" 
 password="tesaka2" 
 server="altair"
 jid = xmpp.JID(user) 
 
-<<<<<<< HEAD
 #è¿”ä¿¡ç”¨ã®é–¢æ•°reply_messã®å®šç¾©
-=======
-#•ÔM—p‚ÌŠÖ”reply_mess‚Ì’è‹`
->>>>>>> fd904b3cedf18d496782c1023722d72c3c01bd1c
 def reply_mess():
 	message	=	raw_input()
 	conn_obj.send( xmpp.Message( mess_node.getFrom(),message)) 
  
-<<<<<<< HEAD
 #ãƒãƒ³ãƒ‰ãƒ©ã®å®šç¾©
 def message_handler(connect_object, message_node): 
 	#ã‚°ãƒ­ãƒ¼ãƒãƒ«å¤‰æ•°ã®å®šç¾©
@@ -51,45 +36,17 @@ def message_handler(connect_object, message_node):
 		thread.daemon = True
 		thread.start()
 		#å—ä¿¡ãƒ¡ãƒƒã‚»ãƒ¼ã‚¸ã‚’æ¨™æº–å‡ºåŠ›ã«è¡¨ç¤º
-=======
-#ƒnƒ“ƒhƒ‰‚Ì’è‹`
-def message_handler(connect_object, message_node): 
-	#ƒOƒ[ƒoƒ‹•Ï”‚Ì’è‹`
-	global mess_body
-	global conn_obj
-	global mess_node
-	#ƒOƒ[ƒoƒ‹•Ï”‚É‘ã“ü
-	mess_body=str(message_node.getBody())
-	conn_obj=connect_object
-	mess_node=message_node
-	#chatstatesŽóMŽž‚É‚àƒnƒ“ƒhƒ‰‚ª“®ì‚·‚é‚Ì‚ÅIf•ªŠò‚Å‚Ó‚é‚¢‚É‚©‚¯‚é
-	if mess_body == "None":
-		pass
-	else:
-		#ƒXƒŒƒbƒh‚ðƒtƒH[ƒN‚µ‚Äreply_messŠÖ”‚ðŽÀs‚³‚¹‚é
-		thread = threading.Thread(target=reply_mess)
-		thread.daemon = True
-		thread.start()
-		#ŽóMƒƒbƒZ[ƒW‚ð•W€o—Í‚É•\Ž¦
->>>>>>> fd904b3cedf18d496782c1023722d72c3c01bd1c
 		sys.stdout.write("\r\n")
 		print mess_body
 		sys.stdout.write("Enter reply:")
 
-<<<<<<< HEAD
 #XMPPã‚µãƒ¼ãƒã«æŽ¥ç¶š
 connection = xmpp.Client(server,debug=[]) 
 #debugãƒ¢ãƒ¼ãƒ‰
-=======
-#XMPPƒT[ƒo‚ÉÚ‘±
-connection = xmpp.Client(server,debug=[]) 
-#debugƒ‚[ƒh
->>>>>>> fd904b3cedf18d496782c1023722d72c3c01bd1c
 #connection = xmpp.Client(server) 
 connection.connect() 
 result = connection.auth(jid.getNode(), password, "cbot-client")
 
-<<<<<<< HEAD
 #messageã‚¹ã‚¿ãƒ³ã‚¶ã‚’å—ä¿¡ã—ãŸã‚‰ãƒãƒ³ãƒ‰ãƒ©ãŒå‹•ä½œã™ã‚‹ 
 connection.RegisterHandler('message', message_handler) 
 
@@ -97,15 +54,6 @@ connection.RegisterHandler('message', message_handler)
 connection.sendInitPresence() 
 
 #Pythonã‚’ç¶™ç¶šç¨¼å‹•ã•ã›ã‚‹ãŸã‚ã«ç„¡é™ãƒ«ãƒ¼ãƒ—
-=======
-#messageƒXƒ^ƒ“ƒU‚ðŽóM‚µ‚½‚çƒnƒ“ƒhƒ‰‚ª“®ì‚·‚é 
-connection.RegisterHandler('message', message_handler) 
-
-#PresenseƒXƒ^ƒ“ƒU‚Ì‘—M
-connection.sendInitPresence() 
-
-#Python‚ðŒp‘±‰Ò“®‚³‚¹‚é‚½‚ß‚É–³ŒÀƒ‹[ƒv
->>>>>>> fd904b3cedf18d496782c1023722d72c3c01bd1c
 while connection.Process(1):
 
 	pass
